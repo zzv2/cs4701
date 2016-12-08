344373cbdfd5daa800e70af77beb755406474669
Zach Z-V <zzv2@cornell.edu>
2016-12-08 01:57:30 -0500

    added poloskys train api call and update figure

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class MplCanvas(FigureCanvas):
 		# We want the axes cleared every time plot() is called
 		self.axes.hold(False)
 
-		self.compute_initial_figure()
+		self.init_epoch_loss_plot()
 		FigureCanvas.__init__(self, fig)
 
 		self.setParent(parent)
@@ -37,31 +37,74 @@ class MplCanvas(FigureCanvas):
 		FigureCanvas.setSizePolicy(self, expanding, expanding)
 		FigureCanvas.updateGeometry(self)
 
-	def compute_initial_figure(self):
-		self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
+	def init_epoch_loss_plot(self):
+		self.epoch_loss_plot([0], [0])
+
+	def epoch_loss_plot(self, x, y, color='r'):
+		self.axes.plot(x,y, color)
 		self.axes.set_xlabel('Epochs')
 		self.axes.set_ylabel('Loss')
 
-	def update_figure(self):
+	def update_figure(self, epochs, loss):
 		# Build a list of 4 random integers between 0 and 10 (both inclusive)
-		l = [ random.randint(0, 10) for i in range(4) ]
-		self.axes.plot([0, 1, 2, 3], l, 'r')
+		n = 100
+		loss = [ random.randint(0, 10) for i in range(n) ]
+		epochs = [i for i in range(n)]
+		self.epoch_loss_plot(epochs, loss)
 		self.draw()
 
 class Main(QMainWindow, Ui_MainWindow):
 	def __init__(self, ):
 		super(Main, self).__init__()
-		self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-		self.setWindowTitle("application main window")
 		self.setupUi(self)
 		self.setupConnections()
+		self.getParams()
+
+		# Setup Cognitive Engine
+		# CognitiveEngine = PoloskysClass()
 
 	def setupConnections(self):
 		self.dc = MplCanvas(self.epoch_loss_plot)
 		QtCore.QObject.connect(self.train_button, QtCore.SIGNAL(_fromUtf8("clicked()")), self.train)
+		# QtCore.QObject.connect(self.neurons_h1, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.callback)
+		# QtCore.QObject.connect(self.neurons_h2, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.callback)
+		# QtCore.QObject.connect(self.sigmoid_function, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.callback)
+		# QtCore.QObject.connect(self.num_epochs, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.callback)
+		# QtCore.QObject.connect(self.learning_rate, QtCore.SIGNAL(_fromUtf8("valueChanged(double)")), self.callback)
+		# QtCore.QObject.connect(self.training_tolerance, QtCore.SIGNAL(_fromUtf8("valueChanged(double)")), self.callback)
+
+	def getParams(self):
+		self._neurons_h1 = self.neurons_h1.value()
+		self._neurons_h2 = self.neurons_h2.value()
+		self._sigmoid_function = str(self.sigmoid_function.currentText())
+		self._num_epochs = self.num_epochs.value()
+		self._learning_rate = self.learning_rate.value()
+		self._training_tolerance = self.training_tolerance.value()
+
+		print "neurons_h1", self._neurons_h1
+		print "neurons_h2", self._neurons_h2
+		print "sigmoid_function", self._sigmoid_function
+		print "num_epochs", self._num_epochs
+		print "learning_rate", self._learning_rate
+		print "training_tolerance", self._training_tolerance
+
 
 	def train(self):
-		self.dc.update_figure()
+		self.getParams()
+		# Run Training Algorithm
+		# epochs, loss = CognitiveEngine.train(
+		# 	_neurons_h1,
+		# 	_neurons_h2,
+		# 	_sigmoid_function,
+		# 	_num_epochs,
+		# 	_learning_rate,
+		# 	_training_tolerance
+		# )
+		# Update Epoch Loss Graph
+
+		# TODO: get epochs, loss
+		epochs, loss = None, None
+		self.dc.update_figure(epochs, loss)
 
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
